2874e9dca3750d147e143c509124a2ec7d30e82f
Stuart Feichtinger <user.email "stuart.feichtinger@gmail.com>
2014-11-17 11:15:54 -0600

    Added header

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+/**************************************************************************/
+/*!
+    @file     main.cpp
+    @author   Stuart Feichtinger
+    @license  MIT (see license.txt)
+
+    Algorithm for computing optimal third-year medical school rotation schedule.
+
+
+    @section  HISTORY
+    v0.0.1 - First release
+
+*/
+/**************************************************************************/
